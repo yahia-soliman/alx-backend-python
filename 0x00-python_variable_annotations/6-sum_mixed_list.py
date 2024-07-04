3e4a887c309ccaf986072c93d4eceb2ddeb15ab9
Yahia <yn7599@gmail.com>
2024-07-04 19:52:06 +0300

    typong with alx

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Complex types - mixed list"""
+"""Complex types - very mixed list"""
 import typing
 
 
-def sum_mixed_list(mxd_list: typing.List[typing.Union[float, int]]) -> float:
+def sum_mixed_list(mxd_list: typing.List[typing.Union[int, float]]) -> float:
     """Get the sum of a list of floats"""
     sum: float = 0
     for n in mxd_list:
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""Complex types - functions from funcitons"""
+"""Complex types - functions from functions"""
 import typing
 
 
-def make_multiplier(multiplyer: float) -> typing.Callable[[float], float]:
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
     """Get a function that multiply by a give multiplyer"""
     def fun(n: float) -> float:
         """Multiply n by multiplyer"""
-        return n * multiplyer
+        return n * multiplier
     return fun
