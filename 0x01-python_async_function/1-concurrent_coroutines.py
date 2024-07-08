7f16ce48c96df94429eec5b857b5e244148da979
Yahia <yn7599@gmail.com>
2024-07-08 18:30:53 +0300

    pycodestyle hate semicolons

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,5 +12,5 @@ async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     coros = [wait_random(max_delay) for _ in range(n)]
     results = []
     for coro in asyncio.as_completed(coros):
-        results.append(await coro);
+        results.append(await coro)
     return results
