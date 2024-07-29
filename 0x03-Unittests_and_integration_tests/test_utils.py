6ef38cb64215a4d55d18b43be9de703bf7dbd29b
Yahia <yn7599@gmail.com>
2024-07-29 22:01:06 +0300

    0x03: assert raises

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test case for nested map"""
         assert access_nested_map(nested_map, path) == expected
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """this shouldn't be legal"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
