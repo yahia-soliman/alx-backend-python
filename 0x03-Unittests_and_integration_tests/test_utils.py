9144a8b84ace882d96b1c56201642b9a9246d3b3
Yahia <yn7599@gmail.com>
2024-07-29 21:26:50 +0300

    0x03: test utils init

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Testing utils for the core utils"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test the functionality of the mentioned function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """test case for nested map"""
+        assert access_nested_map(nested_map, path) == expected
