8ca25ec697bddf0086671ab2bf7f0129e94e72af
Yahia <yn7599@gmail.com>
2024-08-10 16:21:35 +0300

    test docs

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -95,6 +95,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(repos, self.expected_repos)
 
     def test_public_repos_with_license(self):
+        """Test public repos with license"""
         c = GithubOrgClient("google")
         repos = c.public_repos(license="apache-2.0")
         self.assertEqual(repos, self.apache2_repos)
